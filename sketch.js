1cfd047f34068ee64d41f78969f5e437207b1264
Code Sandbox <hello@codesandbox.io>
2023-02-11 04:10:07 +0000

    Deployment from Codesandbox.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
-// declare any variables you need here.
+// The code generates 40 circles with random x and y values between 1 and 500,
+// fill each circle with a random color,
+// and display each circle for a quarter of a second before hiding it and displaying the next one:
+
+// 1.
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-// write functions here that are called in your setup or draw function. please remove this one.
+// 2. Defining a function randomColor that generates a random color in the hexadecimal format.
 function randomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + Math.floor(Math.random() * 16000000).toString(16);
 }
 
+// 3. Defining a function drawCircle that takes x and y as parameters
+// and draws a circle with a radius of 100px at the specified x and y coordinates,
+// filled with a random color.
+// Using the clearRect method to clear the canvas before drawing each circle.
 function drawCircle(x, y) {
-  //call your functions and do your drawing here.
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.beginPath();
   ctx.arc(x, y, 100, 0, 2 * Math.PI);
@@ -17,6 +24,7 @@ function drawCircle(x, y) {
   ctx.stroke();
 }
 
+// 4. Defining a function displayCircles that loops 40 times and sets a timeout of 250 * i milliseconds for each iteration. The setTimeout function calls the drawCircle function with random x and y values between 1 and 400, and passes the current iteration number as the third argument.
 function displayCircles() {
   for (var i = 0; i < 40; i++) {
     setTimeout(
@@ -32,4 +40,5 @@ function displayCircles() {
   }
 }
 
+// 5. Calling the displayCircles function to display the circles. Each circle will be displayed for a quarter of a second before hiding it and displaying the next one
 displayCircles();
